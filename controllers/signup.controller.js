34913b7a2d295fe71e3d30e4db47790f2c08b59c
Abhijith <abhijithreddychinthakuntla@gmail.com>
2024-06-29 13:29:11 +0530

    Made a small change in signup controller

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,36 +1,52 @@
-const {userModel} = require("../models/user.model");
-const {loginModel}=require("../models/login.model")
-const bcrypt =require('bcrypt')
-const jwt=require('jsonwebtoken')
-exports.saveUser=async (req, res)=>{
+const { userModel } = require("../models/user.model");
+const { loginModel } = require("../models/login.model");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.saveUser = async (req, res) => {
     try {
-        console.log(req.body);
-        console.log("Hiii");
-        const { email, password,username, mobileNumber } = req.body;
-        const find=await userModel.findOne({email:{$eq:email}})
-        if(find){
-            
-            res.status(409).json({message:"User already exists.."})
+        const { email, password, username, mobileNumber } = req.body;
+
+        const existingUser = await userModel.findOne({ email: email });
+        if (existingUser) {
+            return res.status(409).json({ message: "User already exists" });
         }
-        const salt=await bcrypt.genSalt(10)
-        const hashedPass=await bcrypt.hash(password,salt)
-        const user1 = await userModel.create({
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt);
+
+        const newUser = await userModel.create({
             email: email,
-            password:hashedPass,
+            password: hashedPass,
             username: username,
             mobileNumber: mobileNumber,
-            ordersList:[]
-            
+            ordersList: []
         });
-        const user2=await loginModel.create({
-            email:email,
-            password:hashedPass,
-            username:username
-        })
-        res.status(200).json({"Valid":"true",message:"SignUp Successful"});
+
+        await loginModel.create({
+            email: email,
+            password: hashedPass,
+            username: username
+        });
+
+        const token = jwt.sign(
+            { id: newUser._id, email: newUser.email, role: 'user' },
+            'medworld',
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({
+            Valid: true,
+            message: "SignUp Successful",
+            token: token,
+            role: 'user',
+            user: {
+                username: newUser.username,
+                _id: newUser._id
+            }
+        });
+    } catch (err) {
+        console.error("Error in saving user:", err);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
     }
-    catch (err) {
-        console.log(err)
-        res.status(404).json({"success":"false", message: "Error in inserting document to user collection" });
-    }
-}
+};
