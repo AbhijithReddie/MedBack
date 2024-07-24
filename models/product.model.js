9f05756fd26dec22a850ff53dd2422de44d93e7a
Abhijith <abhijithreddychinthakuntla@gmail.com>
2024-07-24 11:17:44 +0530

    random12344

--- models/product.model.js
+++ models/product.model.js
@@ -1,38 +1,44 @@
 const mongoose = require("mongoose");
+
 const productSchema = mongoose.Schema({
-    productId:{
-        type:String,
-        required:true
+    productId: {
+        type: String,
+        required: true
     },
-    imageUrl:{
-        type:String,
-        required:true
+    imageUrl: {
+        type: String,
+        required: true
     },
-    productName:{
-        type:String,
-        required:true
+    productName: {
+        type: String,
+        required: true
     },
-    price:{
-        type:Number,
-        required:true
+    price: {
+        type: Number,
+        required: true
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    quantity:{
-        type:Number,
-        default:0,
-        required:true
+    quantity: {
+        type: Number,
+        default: 0,
+        required: true
     },
-    prescriptionRequired:{
-        type:Boolean,
-        required:true
+    prescriptionRequired: {
+        type: Boolean,
+        required: true
     },
-    checkAvailable:{
-        type:String,
-        default:"Availble"
+    checkAvailable: {
+        type: String,
+        default: "Available"
+    },
+    categories: {
+        type: [String],
+        required: true
     }
 });
+
 const productModel = mongoose.model("product", productSchema);
-module.exports = {productModel,productSchema};
+module.exports = { productModel, productSchema };
--- routes/home.route.js
+++ routes/home.route.js
@@ -3,7 +3,7 @@ const homeroutes=express.Router()
 const controller=require('../controllers/product.controller')
 const cartControl=require('../controllers/cart.controller')
 const {authenticateUser} =require('../middlewares/user.middleware')
-homeroutes.post('/',controller.getHomeProduct)
+homeroutes.get('/',controller.getHomeProduct)
 homeroutes.post('/getProd/:id',controller.getProdData)
 homeroutes.post('/:id',authenticateUser,cartControl.addToCart)
 module.exports=homeroutes;
